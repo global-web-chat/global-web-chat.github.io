69d70e28635633ae7920101712e20c3f0bfb9d6f
global-web-chat <72561813+global-web-chat@users.noreply.github.com>
2020-10-08 20:45:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
-
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('global-web-chat').then(function(cache) {
+        return cache.addAll([
+          'https://global-web-chat.github.io/',
+          'index.html',
+          'script.js',
+          'style.css',
+          'cover.svg',
+          'global-web-chat.png',
+          'app.webmanifest',
+          'about-us/',
+          'contact-us/',
+          'privacy-policies/',
+          'terms-of-services/',
+        ]);
+      })
+    );
+   });
+   
+   self.addEventListener('fetch', function(e) {
+     console.log(e.request.url);
+     e.respondWith(
+       caches.match(e.request).then(function(response) {
+         return response || fetch(e.request);
+       })
+     );
+   });
+   
+   
