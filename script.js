46438027922778fa32bef20ad1a15b8be71f26fc
global-web-chat <72561813+global-web-chat@users.noreply.github.com>
2020-10-08 20:47:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+// Register service worker to control making site work offline
+
+if('serviceWorker' in navigator) { navigator.serviceWorker .register('sw.js') .then(function() { console.log('Service Worker Registered'); }); } 
+ 
